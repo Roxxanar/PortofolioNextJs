8a9e19ad9a50a2ded34c985645f1459eea895de2
Roxana <mara-roxana.rusu@student.tuiasi.ro>
2025-06-15 23:01:04 +0300

    programming3

--- src/app/programming/page.tsx
+++ src/app/programming/page.tsx
@@ -106,8 +106,8 @@ export default function Home() {
 
 <div className={styles.info_container}>
 
-  <div className={`${styles.project_container} ${squarePeg.className}`}>
-    <div className={styles.p_name}>2. Clothes Website Frontend</div>
+  <div className={`${styles.project_container} ${squarePeg.className} ${styles.cwfrontend}`}>
+    <div className={`${styles.p_name}`}>2. Clothes Website Frontend</div>
     <div className={styles.p_socials}>
       <Image
                         aria-hidden
@@ -122,7 +122,7 @@ export default function Home() {
   </div>
   <div className={`${styles.description_container} ${squarePeg.className}`}>
   <div className={styles.p_description}>
-    This website, made with Next.js framework. Design made by me with some components and animations inspired from codepen.io and uiverse.io
+  Frontend of a personal project, a website for clothes shopping made with Angular framework. The project is unfinished but it has some caracteristhics, including seach filter, load more button etc.
   </div>
   </div>
 </div>
@@ -167,8 +167,8 @@ export default function Home() {
 
 <div className={styles.info_container}>
 
-  <div className={`${styles.project_container} ${squarePeg.className}`}>
-    <div className={styles.p_name}>3. Clothes Website Backend</div>
+  <div className={`${styles.project_container} ${squarePeg.className} ${styles.cwfrontend}`}>
+    <div className={`${styles.p_name} `}>3. Clothes Website Backend</div>
     <div className={styles.p_socials}>
       <Image
                         aria-hidden
@@ -183,14 +183,14 @@ export default function Home() {
   </div>
   <div className={`${styles.description_container} ${squarePeg.className}`}>
   <div className={styles.p_description}>
-    This website, made with Next.js framework. Design made by me with some components and animations inspired from codepen.io and uiverse.io
+    Backend for the previous project, made with Node.js and Supabase postgresql database
   </div>
   </div>
 </div>
 
 <div className={styles.percent_container}>
 
-<div className={styles.percent_container2}>
+<div className={`${styles.percent_container2} ${styles.js_percent}`}>
       <div className={styles.skillBox}>
         <span className={styles.title}>JavaScript</span>
         <div className={styles.skillBar}>
@@ -215,6 +215,78 @@ export default function Home() {
 <div className={styles.loader}></div>
 
 
+<div className={styles.site_container}>
+
+<div className={styles.info_container}>
+
+  <div className={`${styles.project_container} ${squarePeg.className} ${styles.cwfrontend}`}>
+    <div className={`${styles.p_name}`}>4. StudentVoice WebApp</div>
+    <div className={styles.p_socials}>
+      <Image
+                        aria-hidden
+                        src="/github (1).png"
+                        alt="Globe icon"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{ width: "100%", height: "auto" }}
+                      />
+    </div>
+  </div>
+  <div className={`${styles.description_container} ${squarePeg.className}`}>
+  <div className={styles.p_description}>
+  A web application made with Angular framework and .NET technology, both frontend and backend. <br></br>
+  The project was made in the Summer Practice program and it is an application that allow students to share feedback on different topics. 
+  <br></br>Above you can find the work-flow, wireframes in Figma and Github link 
+  </div>
+  </div>
+</div>
+
+<div className={styles.percent_container}>
+
+<div className={`${styles.percent_container2} ${styles.fourth_percent}`}>
+      <div className={styles.skillBox}>
+        <span className={styles.title}>HTML</span>
+        <div className={styles.skillBar}>
+          <span className={`${styles.skillPerhtml} ${styles.first}`}>
+            <span className={styles.tooltip}>49.1%</span>
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.skillBox}>
+        <span className={styles.title}>SCSS</span>
+        <div className={styles.skillBar}>
+          <span className={`${styles.skillPerscss} ${styles.second}`}>
+            <span className={styles.tooltip}>32.3%</span>
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.skillBox}>
+        <span className={styles.title}>TypeScript</span>
+        <div className={styles.skillBar}>
+          <span className={`${styles.skillPerbootstrap} ${styles.third}`}>
+            <span className={styles.tooltip}>32.3%</span>
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.skillBox}>
+        <span className={styles.title}>C#</span>
+        <div className={styles.skillBar}>
+          <span className={`${styles.skillPerbootstrap} ${styles.fourth}`}>
+            <span className={styles.tooltip}>49.1%</span>
+          </span>
+        </div>
+      </div>
+
+
+    </div>
+</div>
+
+
+</div>
 
 
 </div>
